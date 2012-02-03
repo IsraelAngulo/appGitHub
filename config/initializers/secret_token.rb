017be994671b43a75d92c1bf1f333f059f8ba70c
IsraelAngulo <perpgria@gmail.com>
2012-02-03 14:41:29 -0500

    Copia de otra aplicaciom

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AppChat::Application.config.secret_token = '2a6940980fd7346b1528c5156df976e7d8a3d5d56826978193bd311a033998ac200da9d4921b3f1991804f7bdb2fb1a3a3ca751a654ad27e323ca36cb59136cf'
